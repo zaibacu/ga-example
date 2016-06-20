efdc1c439cd8a434026b4f1287ccbc3ddcef53d3
Šarūnas Navickas <sarunas.navickas@adform.com>
2016-06-20 07:55:52 +0300

    Dynamic fitness function

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import click
 
 
-def fitness(dnr: list) -> int:
+def manual_fitness(dnr: list) -> int:
     print(dnr)
     score = input("Enter score> ")
     return int(score)
@@ -12,9 +12,9 @@ def random_dnr(size: int, seq: list) -> list:
     return [choice(seq) for _ in range(0, size)]
 
 
-def evaluate(population: list) -> list:
+def evaluate(population: list, fitness_fn: callable) -> list:
     for pop in population:
-        yield fitness(pop), pop
+        yield fitness_fn(pop), pop
 
 
 def mix(dnr1: list, dnr2: list, size: int) -> list:
@@ -31,8 +31,8 @@ def mutate(dnr: list, seq: list):
         dnr[r] = choice(seq)
 
 
-def evolution(population, size, seq, limit):
-    top = sorted(evaluate(population), key=lambda x: x[0], reverse=True)[:limit]
+def evolution(population, fitness_fn, size, seq, limit):
+    top = sorted(evaluate(population, fitness_fn), key=lambda x: x[0], reverse=True)[:limit]
     print(top)
     population.clear()
     for i in range(0, len(top)):
@@ -48,15 +48,25 @@ def evolution(population, size, seq, limit):
 @click.command()
 @click.option("--size", help="Sequance length", type=int)
 @click.option("--population_size", help="Population size", type=int, default=6)
+@click.option("--fitness_fn", help="Fitness function", type=str, default="__main__.manual_fitness")
 @click.argument("seq", nargs=-1)
-def main(size, population_size, seq):
+def main(size, population_size, fitness_fn,  seq):
+
     population = [random_dnr(size, seq)
                   for i in range(0, population_size)]
 
+    def get_function():
+        buff = fitness_fn.split(".")
+        mod = ".".join(buff[:-1])
+        fn = buff[-1]
+        m = __import__(mod)
+        return getattr(m, fn)
+
+    fitness = get_function()
     iteration = 1
     while True:
         print("iteration: {0}".format(iteration))
-        evolution(population, size, seq, int(population_size / 2))
+        evolution(population, fitness, size, seq, int(population_size / 2))
         iteration += 1
 
 if __name__ == "__main__":
